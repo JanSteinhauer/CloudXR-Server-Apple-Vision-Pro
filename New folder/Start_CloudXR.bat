fa3400e6ae7a59c8d583e066cbc0df4d03f78a42
Jan Steinhauer <jan.steinhauer@stud-mail.uni-wuerzburg.de>
2026-05-12 14:37:12 +0200

    feat: added Project that can Stream XR

--- New folder/Start_CloudXR.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Setting OpenXR Runtime to NVIDIA CloudXR...
-set "XR_RUNTIME_JSON=C:\MasterThesisJanSteinhauer\ClaudeXR-Server-Apple-Vision-Pro\Stream-Manager-6.0.4-win64 - Copy\Server\releases\6.0.5\openxr_cloudxr.json"
-
-echo Starting Unity Game...
-start "" "CloudXRStreamingTest.exe"
-
-echo Game launched! Waiting for connection...
-pause
